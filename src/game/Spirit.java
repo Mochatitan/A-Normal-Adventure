851039d181bf49b77290e86f503b788a14c62925
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-18 10:46:53 -0400

    Update Spirit.java

--- src/game/Spirit.java
+++ src/game/Spirit.java
@@ -1,12 +1,14 @@
 package game;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class Spirit{
+  
 protected String name;
 protected int index;
-protected int level;
-  
+
 protected int maxHealth;
 protected int health;
 protected int damage;
@@ -18,8 +20,15 @@ protected double size;
 
 protected Personality personality;
   
-
+protected int level;
+protected double totalExp;
+protected double exp;
+protected ArrayList<double> expRequirement = new ArrayList<double>();
   
+  
+public Pokemon(){
+loadVariables();
+}
   
 //getters and setters
 public String getName(){
@@ -31,5 +40,41 @@ public int getIndex(){
 public int getLevel(){
 return level;
 }
+public void setLevel(int newLevel){
+level = newLevel;
+}
+public void addLevels(int levelsAdded){
+level += levelsAdded;
+}
+public void levelUp(){
+level++;
+}
+private int calculateLevel(){
+while(exp > expRequirements.get(level)){
+exp -= expRequirements.get(level);
+level++;
+}
+}
+
+//loaders
+private void loadVariables(){
+loadExpRequirement();
+
+  
+
+}
+private void loadExpRequirement(){
+//expRequirement = new ArrayList<int>(Arrays.asList(0,2,3,4,4,5,7,));
+
+private double xpReq = 1;
+for(double x = 0; x < 98;x+=0.5){
+expRequirement.add(xpReq);
+xpReq += x;
+}
+
+}
+  
+
+
 
 }
