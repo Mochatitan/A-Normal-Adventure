639e704e8294fe30d547a9eeeece7a03f76a45be
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-24 10:45:06 -0400

    Create Attack.java

---
+++ src/game/Attack.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Attack{
+  
+public int baseDamage;
+protected int uses;
+protected int maxUses;
+protected Type type;
+protected String name = this.getClass().getSimpleName();
+protected String description;
+
+}
