547b2ff6a5affda0cb68c7d51b75dd6b725f76fa
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-24 10:16:40 -0400

    Create Battle.java

---
+++ src/game/Battle.java
@@ -0,0 +1,17 @@
+package game;
+
+import javax.swing.*;
+
+
+public class Battle extends JFrame{
+
+  public Battle(){
+    initUI();
+  }
+  public void initUI(){
+setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+
+}
