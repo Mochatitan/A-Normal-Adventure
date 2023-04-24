be52bc7196a8715d766bc3060c04ade99067a04e
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-24 11:41:07 -0400

    Update Attack.java

--- src/game/Attack.java
+++ src/game/Attack.java
@@ -6,8 +6,8 @@ public int baseDamage;
 protected int uses;
 protected int maxUses;
 protected Type type;
-protected String name = this.getClass().getSimpleName();
-protected String description;
+protected String name;
 Attack(){
+name = this.getClass().getSimpleName();
 uses = maxUses;
 }
