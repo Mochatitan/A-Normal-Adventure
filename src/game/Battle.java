00f739dd698d8c21abdad2eac21c5c0cb45d051b
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-25 10:43:34 -0400

    Update Battle.java

--- src/game/Battle.java
+++ src/game/Battle.java
@@ -4,16 +4,21 @@ import javax.swing.*;
 
 
 public class Battle extends JFrame{
-  public Spirit spirit1;
-  public Spirit spirit2;
+  public static final double CRIT_CHANCE = 0.05;
+  public Spirit spirit1 = new Spirit("attackingPok",10, 20, 5, Type.FIRE);
+  public Spirit spirit2 = new Spirit("defendingpok",12,18,4,Type.GRASS);
   
   public Battle(){
     initUI();
   }
   public void initUI(){
-setSize(300, 200);
+        setSize(300, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+  public double calculateDamage(Spirit attackingSpirit, Spirit defendingSpirit, Attack attackUsed){
+    boolean isCrit = randomBoolean(Battle.CRIT_CHANCE);
+
   }
 
 }
