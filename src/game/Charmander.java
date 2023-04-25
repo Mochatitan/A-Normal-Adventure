1d0d07a4cdd72410feff679314505db22c96a194
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-25 13:02:57 -0400

    Create Charmander.java

---
+++ src/game/Charmander.java
@@ -0,0 +1,11 @@
+package game;
+
+//for reference
+public class Charmander extends Spirit{
+  this.maxHealth = 39;
+  this.attack = 52;
+  this,defense = 43;
+  this.speed = 65;
+  this.type = Type.FIRE;
+  
+  
