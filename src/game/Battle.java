31bf69161a4e9c24c5e856ca801cdd7844012270
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-25 12:59:12 -0400

    Update Battle.java

--- src/game/Battle.java
+++ src/game/Battle.java
@@ -18,14 +18,18 @@ public class Battle extends JFrame{
   }
   public double calculateDamage(Spirit attackingSpirit, Spirit defendingSpirit, Attack attackUsed){
     boolean isCrit = randomBoolean(Battle.CRIT_CHANCE);
+    //1 if it doesnt crit, 2 if it crits.
     int critical = 1;if(isCrit){critical = 2;}
+    //level of the attacking spirit
     int level = attackingSpirit.getLevel();
-    int power = "the power of the used move";
+    //power of the used move.
+    int power = attackUsed.getPower();
+    //type effectiveness of the attack vs the defending pokemon.scales from 0.5, 1, 2.
     int typeEffectiveness = 1;
-    int typeEffectiveness2 = 1;
+    //todo
     int random = (217-255);
     
-    double damage = (((2*level*critical)*power*attack/defense)/50)*typeEffectiveness*typeEffectiveness2*random;
+    double damage = (((2*level*critical)*power*attack/defense)/50)*typeEffectiveness*random;
     return damage;
   }
 
