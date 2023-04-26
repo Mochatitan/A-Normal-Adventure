18420f5f849a6a5fd65b1e59c1a732a786b5e882
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-26 10:20:49 -0400

    Updated file and fixed syntax

--- src/game/SpiritLoader.java
+++ src/game/SpiritLoader.java
@@ -1,19 +1,18 @@
 package game;
 import java.io.File;
 // Import Jackson json databind/core
-//Import HashMap
-//Import Spirit class
+import java.util.HashMap;
 
 public class SpiritLoader{
-     HashMap<String, Spirit> spirits = new HashMap
+     HashMap<String, Spirit> spirits = new HashMap<>();
      // Unsure exactly how you want this handled, but as a proof of concept and a good starting point
      // I am going to just create of array of all available Spirits dymaically based on availble json files
      ObjectMapper objectMapper = new ObjectMapper();
 
-     File dir = new File("objects/spirits/");
+     File dir = new File("data/objects/spirits/");
      File[] directoryListing = dir.listFiles();
 
-     publc void loadSpirits(){
+     public void loadSpirits(){
 
           if(directoryListing != null) {
                for(File child : directoryListing) {
