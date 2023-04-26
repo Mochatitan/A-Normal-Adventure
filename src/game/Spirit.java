665bf4211c3cbe5ea6dc6e228dc2f69f0211bc58
Joshua <joshbuker04@gmail.com>
2023-04-25 18:35:03 -0700

    Added json to spirit loader

--- src/game/Spirit.java
+++ src/game/Spirit.java
@@ -4,81 +4,91 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public abstract class Spirit{
-  
-protected String name;
-protected int index;
+public abstract class Spirit {
 
-protected int maxHealth;
-protected int health;
-protected int damage;
-protected int defense;
+    protected String name;
+    protected int ID;
+    protected String description;
 
-protected double height;
-protected double weight;
-protected double size;
+    protected Type type1;
+    protected Type type2;
 
-protected Personality personality;
-  
-protected int level;
-protected double totalExp;
-protected double exp;
-protected ArrayList<Double> expRequirements = new ArrayList<Double>();
-  
-  
-public Spirit(){
-loadVariables();
-}
-  
-//getters and setters
-public String getName(){
-return name;
-}
-public int getIndex(){
-  return index;
-}
-public int getLevel(){
-return level;
-}
-public void setLevel(int newLevel){
-level = newLevel;
-}
-public void addLevels(int levelsAdded){
-level += levelsAdded;
-}
-public void levelUp(){
-level++;
-}
-private int calculateLevel(){
-while(exp > expRequirements.get(level)){
-exp -= expRequirements.get(level);
-level++;
-}
-return level;
-}
+    protected int maxHealth;
+    protected int health;
+    protected int damage;
+    protected int defense;
 
-//loaders
-private void loadVariables(){
-loadExpRequirement();
-loadInformation();
-}
-  
-private void loadExpRequirement(){
+    protected double height;
+    protected double weight;
+    protected double size;
+
+    protected Personality personality;
+
+    protected int level;
+    protected double totalExp;
+    protected double exp;
+    protected ArrayList<Double> expRequirements = new ArrayList<Double>();
+
+
+    public Spirit() {
+        loadVariables();
+    }
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int newLevel) {
+        level = newLevel;
+    }
+
+    public void addLevels(int levelsAdded) {
+        level += levelsAdded;
+    }
+
+    public void levelUp() {
+        level++;
+    }
+
+    private int calculateLevel() {
+        while (exp > expRequirements.get(level)) {
+            exp -= expRequirements.get(level);
+            level++;
+        }
+        return level;
+    }
+/*
+    //loaders
+    private void loadVariables() {
+        loadExpRequirement();
+        loadInformation();
+    }
+
+    private void loadExpRequirement() {
 //expRequirement = new ArrayList<int>(Arrays.asList(0,2,3,4,4,5,7,));
-double xpReq = 1;
-for(double x = 0; x < 98;x+=0.5){
-expRequirements.add(xpReq);
-xpReq += x;
-}
-}
+        double xpReq = 1;
+        for (double x = 0; x < 98; x += 0.5) {
+            expRequirements.add(xpReq);
+            xpReq += x;
+        }
+    }
 
-private void loadInformation(){
-this.name = this.getClass().getSimpleName();
-}
-  
-private void randomizeStats(){
+    private void loadInformation() {
+        this.name = this.getClass().getSimpleName();
+    }
 
-}
+    private void randomizeStats() {
 
+    }
 
+*/
 }
--- src/game/SpiritLoader.java
+++ src/game/SpiritLoader.java
@@ -1,5 +1,28 @@
 package spirits;
+import java.io.File;
+// Import Jackson json databind/core
+//Import HashMap
+//Import Spirit class
 
 public class SpiritLoader{
-     private TxtReader t = new TxtReader(String "spirits");
+     HashMap<String, Spirit> spirits = new HashMap
+     // Unsure exactly how you want this handled, but as a proof of concept and a good starting point
+     // I am going to just create of array of all available Spirits dymaically based on availble json files
+     ObjectMapper objectMapper = new ObjectMapper();
+
+     File dir = new File("objects/spirits/");
+     File[] directoryListing = dir.listFiles();
+
+     publc void loadSpirits(){
+
+          if(directoryListing != null) {
+               for(File child : directoryListing) {
+                    Spirit spirit = objectMapper.readValue(child, Spirit.class);
+                    spirits.put(spirit.getName(), spirit);
+               }
+          }
+     }
+
+
+
 }
