c0039a6ede2b7fa4917b8a35e36c8fe3a25a6bc5
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-27 10:56:29 -0400

    Create hasLoadingScreen.java

---
+++ src/main/java/org/mochatitan/interfaces/hasLoadingScreen.java
@@ -0,0 +1,10 @@
+package org.mochatitan.interfaces;
+
+import java.awt.Graphics2D;
+
+public interface hasLoadingScreen{
+  
+  public void loadingTick();
+  public void loadingDraw(Graphics2D g2d);
+  
+}
