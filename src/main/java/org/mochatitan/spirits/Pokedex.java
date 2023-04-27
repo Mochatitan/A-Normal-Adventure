bf33d05f5daf1f2c45b5cc95e4b6298c72fd69c9
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-04-27 11:46:22 -0400

    Added Pokédex with all kanto pokemon

--- src/main/java/org/mochatitan/spirits/Pokedex.java
+++ src/main/java/org/mochatitan/spirits/Pokedex.java
@@ -26,19 +26,33 @@ public Pokedex(){
 turnNamesToNumbers();
 addPokemonToPokedex();
 }
-
-public void addPokemonToPokedex(){
+  /**
+  *@param index the pokemons index, dont subtract 1 for lists, thats in the method.
+  */
+  public Pokemon get(int index){
+    return pokedex.get(index-1);
+  }
+  public Pokemon get(String name){
+    int index = dex.get(name);
+    return pokedex.get(index-1);
+  }
+  
+private void addPokemonToPokedex(){
 
 }
-public void turnNamesToNumbers(){
+private void turnNamesToNumbers(){
 addToDex(“Bulbasaur”,“Ivysaur”,“Venusaur”,“Charmander”,“Charmeleon”,“Charizard”,“Squirtle”,“Wartortle”,“Blastoise”,“Caterpie”,“Metapod”,“Butterfree”,“Weedle”,“Kakuna”,“Beedrill”,“Pidgey”,“Pidgeotto”,“Pidgeot”,“Rattata”,“Raticate”,“Spearow”,“Fearow”,“Ekans”,“Arbok”,“Pikachu”,“Raichu”,“Sandshrew”,“Sandslash”,“Nidoran♀”,“Nidorina”,“Nidoqueen”,“Nidoran♂”,“Nidorino”,“Nidoking”,“Clefairy”,“Clefable”,“Vulpix”,“Ninetales”,“Jigglypuff”,“Wigglytuff”,“Zubat”,“Golbat”,“Oddish”,“Gloom”,“Vileplume”,“Paras”,“Parasect”,“Venonat”,“Venomoth”,“Diglett”,“Dugtrio”,“Meowth”,“Persian”,“Psyduck”,“Golduck”,“Mankey”,“Primeape”,“Growlithe”,“Arcanine”,“Poliwag”,“Poliwhirl”,“Poliwrath”,“Abra”,“Kadabra”,“Alakazam”,“Machop”,“Machoke”,“Machamp”,“Bellsprout”,“Weepinbell”,“Victreebel”,“Tentacool”,“Tentacruel”,“Geodude”,“Graveler”,“Golem”,“Ponyta”,“Rapidash”,“Slowpoke”,“Slowbro”,“Magnemite”,“Magneton”,“Farfetch’d”,“Doduo”,“Dodrio”,“Seel”,“Dewgong”,“Grimer”,“Muk”,“Shellder”,“Cloyster”,“Gastly”,“Haunter”,“Gengar”,“Onix”,“Drowzee”,“Hypno”,“Krabby”,“Kingler”,“Voltorb”,“Electrode”,“Exeggcute”,“Exeggutor”,“Cubone”,“Marowak”,“Hitmonlee”,“Hitmonchan”,“Lickitung”,“Koffing”,“Weezing”,“Rhyhorn”,“Rhydon”,“Chansey”,“Tangela”,“Kangaskhan”,“Horsea”,“Seadra”,“Goldeen”,“Seaking”,“Staryu”,“Starmie”,“Mr. Mime”,“Scyther”,“Jynx”,“Electabuzz”,“Magmar”,“Pinsir”,“Tauros”,“Magikarp”,“Gyarados”,“Lapras”,“Ditto”,“Eevee”,“Vaporeon”,“Jolteon”,“Flareon”,“Porygon”,“Omanyte”,“Omastar”,“Kabuto”,“Kabutops”,“Aerodactyl”,“Snorlax”,“Articuno”,“Zapdos”,“Moltres”,“Dratini”,“Dragonair”,“Dragonite”,“Mewtwo”,“Mew”);
 }
 
-  public void addToDex(String...name){
+  private void addToDex(String...name){
     for(int n = 1; n <= name[].length; n++){
       dex.put(name[n-1],n);
+      dex.put(name(n-1].toLowerCase(),n);
+      pokedex.add(pokeApi.getPokemon(n));
     }
   }
-
+  public void getLength(){
+return pokedex.length;
+  }
   
 }
